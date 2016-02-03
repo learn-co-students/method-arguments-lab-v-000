b46c86be4795feb095b8b17e38bb4143aa93e0ed
Chris-Sexton <champlain2013@gmail.com>
2016-02-03 09:38:10 -0500

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+#lib/introduction.rb
+def introduction(name=name)
+  puts "Hi, my name is #{name}."
+end
+
+#lib/introduction_with_language.rb
+def introduction_with_language(name=name, lang=language)
+  puts"Hi, my name is #{name} and I am learning to program in #{lang}."
+end  
