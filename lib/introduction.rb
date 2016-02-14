c158da17360e343bc9c37eaee8d5ee38d3ae2998
Lady Codington <myemailforcoding@gmail.com>
2016-02-13 21:03:28 -0500

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(thing)
+  puts "Hi, my name is #{thing}.\n"
+end
+
+def introduction_with_language(thing1, thing2)
+  puts "Hi, my name is #{thing1} and I am learning to program in #{thing2}.\n"
+end
