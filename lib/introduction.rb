923dd121f14a56f6635205cb28c0ed3733ea28cc
jack woodward <jackwoodwardschool@gmail.com>
2016-05-16 11:38:25 -0700

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction(name)
+    puts("Hi, my name is #{name}.\n")
+end
+
+introduction("Josh")
+
+def introduction_with_language(name, language)
+    puts("Hi, my name is #{name} and I am learning to program in #{language}.\n")
+end
+
+introduction_with_language("Dan", "Ember.js")
