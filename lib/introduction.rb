474177213712a9ea49e13646c0e30573994fc999
tkanen <tina.kane@gmail.com>
2016-07-07 11:43:00 -0700

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+introduction("Josh")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
