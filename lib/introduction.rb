a3a17a46d6fe566fbf86d19c64baba8d7aad9933
Elvin <elvin@elvingarcia.com>
2016-07-06 21:47:56 -0400

    I'm really Done now

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is Dan and I am learning to program in Ember.js.\n"  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"  
 end
