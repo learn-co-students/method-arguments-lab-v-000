9a49a1ceabd29889472a155f3db6df08dca9f20c
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-07-21 19:04:09 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def introduction(sholom)
+  puts "Hi, my name is #{sholom}."
+end
+
+
+def introduction_with_language(name, lang)
+  puts "Hi, my name is #{name} and I am learning to program in #{lang}."
+end
