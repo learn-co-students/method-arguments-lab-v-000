f81b50b9f4c6d47afb66da5d93adf58f45489805
Mr. Mueller <bmueller@hstat.org>
2016-09-28 21:02:14 +0000

    second test case for #introduction

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe "#introduction" do 
   it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
+    expect{introduction("Jill")}.to output("Hi, my name is Jill.\n").to_stdout
   end
 end
 
