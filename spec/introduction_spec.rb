afb0e7b42ac9902c0491e079fe9d626c2a5d4ef9
Mr. Mueller <bmueller@hstat.org>
2016-09-28 21:14:49 +0000

    second test case for #introduction_with_language

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -9,6 +9,7 @@ end
 
 describe "#introduction_with_language" do 
   it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do 
-    expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
+    expect{introduction_with_language("Richard", "Ruby")}.to output("Hi, my name is Richard and I am learning to program in Ruby.\n").to_stdout
+    expect{introduction_with_language("Patricia", "Python")}.to output("Hi, my name is Patricia and I am learning to program in Python.\n").to_stdout
   end
 end
