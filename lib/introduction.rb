3cbd0cebc437fa65828c73b3a1031844bf2906f4
Elias Ventura <eliasventura23@gmail.com>
2016-11-03 00:27:11 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
 
-def introduction (name)
-  puts "Hi, my name is #{name}."
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
