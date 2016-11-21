196bff0b5dd2b40cce35f71f53378ee3bb2117ae
Thomas Hines <thomashines@gmail.com>
2016-11-21 17:34:19 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+# introduction("Josh")}.to output("Hi, my name is Josh.\n"
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+# introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
