7dcfeba191f0a69754c6cb64ff1d9894af364fd5
Trai Compton <tlcompton2546@outlook.com>
2017-02-05 18:47:10 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+#INTRODUCTION
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+#INTRODUCTION_WITH_LANGUAGE
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
