ab57d986f9c03b8e59053866aabdc8409b306f3e
Nicholas D'Amico <nickalan82@icloud.com>
2017-05-22 21:50:42 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+# Create method called #introduction( arg1 )
+  # Takes in a variable name and outputs a string with that name using interpolation.
+  def introduction(name)
+    puts "Hi, my name is #{name}."
+  end
+
+  def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  end
