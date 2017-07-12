01a4442c3091b0f277f740789498076a9e92126d
Dominique De Leon <dominique.a.deleon@gmail.com>
2017-07-12 19:12:59 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+# name = "Josh"
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+# name="Dan"
+# language="Ember.js"
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
