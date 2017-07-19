98da19b07059cefce37ef7fdb792c41b63bfddc1
lnbatioco <LoryleenBatioco@gmail.com>
2017-07-18 22:16:28 -0700

    add introduction and introduction_with_language methods

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+introduction("Lory")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I'm learning to program in #{language}."
+end
+introduction_with_language("Lory", "Ruby")
