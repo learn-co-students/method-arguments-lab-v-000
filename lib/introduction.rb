e7f52c0e486895c2c5cb0bbe68e9ffc831e2b2cf
Fiona Lapham <fionadlapham@gmail.com>
2017-10-15 02:48:30 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(josh)
+  puts "Hi, my name is #{josh}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
