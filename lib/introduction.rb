8207f11838a491795018543a124db7cd6484547f
Daiane Bushey <daianebushey@gmail.com>
2017-12-21 17:22:52 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,5 +4,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
