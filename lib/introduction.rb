0ef7166927b979b58956a4a9a5c2e6157188c235
Natalie Halawani <hala1224@gmail.com>
2017-12-26 00:03:40 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,2 +1,6 @@
-var greeting
-greeting = "Hello World"
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+def introduction_with_language(name,language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
