2e6f9efc87ab06b230999d8ea02efe9581caa608
Jordan Kline <jordan.kline@gmail.com>
2018-01-04 00:43:54 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}.\n"
+end
+
+def introduction_with_language(name, thing)
+  puts "Hi, my name is #{name} and I am learning to program in #{thing}.\n"
+end
