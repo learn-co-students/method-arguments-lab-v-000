4f1a9c17e3ba427346b0c43d59673345408b20bb
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-02-03 09:26:16 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}"
+end
+
+def introduction_with_language(name, language)
+  puts  "Hi, my name is #{namea} and I am learning to program in #{language}"
+end
