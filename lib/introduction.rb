25d4cfd28b6774bf0f4465c72ca0dcc7c0d3942c
mackdalton <mackdalton@gmail.com>
2018-03-03 12:17:20 -0800

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  name = "Dan"
-  language = "Ember.js"
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
