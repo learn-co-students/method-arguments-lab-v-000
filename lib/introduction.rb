2371128a947e88ccd334b3dd54db377900eb35b7
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 17:57:58 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ def introduction(name)
 end
 
 ##
-# introduction_with_language takes in a name and a language and outputs and introduction with the name and language (using string interpolation)
+# #introduction_with_language takes in a name and a language and outputs and introduction with the name and language (using string interpolation)
 ##
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
