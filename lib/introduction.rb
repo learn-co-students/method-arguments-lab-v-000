3ef211eea4b49257a0b1ee250a3fe77c2c480695
Sharmaine Requerme <srqrme@gmail.com>
2018-07-19 17:15:04 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,10 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+introduction("Sharmaine")
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+introduction_with_language("Sharmaine", "Ruby")
