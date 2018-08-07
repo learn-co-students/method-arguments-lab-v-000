2417a9ec4baee19942b73161b7513b3d1dc2c03d
Yolanda Fay <yolandafay@gmail.com>
2018-08-07 01:45:23 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+#introduction method #takes one arg and outputs a phrase using it
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+#introduction_with_language method #takes two args and outputs a phrase using both
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
