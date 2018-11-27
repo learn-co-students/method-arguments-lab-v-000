4fc51aed7b5bfe71feb4120220889b9d6764eb2e
Toni-Ty <antonique2479@yahoo.com>
2018-11-27 21:34:22 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{langauge}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
   end
-end
