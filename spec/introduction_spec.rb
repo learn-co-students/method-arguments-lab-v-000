7cc2e5a58eba89c5740372109f3e81f84f13507e
Brandon Brasson <rocksong622@gmail.com>
2019-01-04 18:52:36 +0000

    Done.

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -1,12 +1,12 @@
 require "spec_helper"
 
-puts describe "#introduction" do
+ describe "#introduction" do
   it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do
     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
   end
 end
 
-puts describe "#introduction_with_language" do 
+ describe "#introduction_with_language" do
   it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do
     expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
   end
